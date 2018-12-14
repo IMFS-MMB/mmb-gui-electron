9a5f46da9d919a688ab841b7afc06e414503d134
Jan Lohage <git@j2L4e.de>
2018-12-14 15:39:37 +0100

    remove workaround for badly formatted json files

--- src/renderer/utils/electron/read-json-file.js
+++ src/renderer/utils/electron/read-json-file.js
@@ -3,10 +3,8 @@ import { promisify } from 'util';
 
 const readFile = promisify(fs.readFile);
 
-export default async function readJsonFile(path, dirty = false) {
+export default async function readJsonFile(path) {
   const contents = await readFile(path, { encoding: 'utf8' });
 
-  // todo: remove eval when output JSON is standard compliant.
-  // eslint-disable-next-line no-eval
-  return dirty ? eval(contents) : JSON.parse(contents);
+  return JSON.parse(contents);
 }
