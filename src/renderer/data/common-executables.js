1e263e04a4c399235d9f0dfa718f3dc2d5759999
Jan Lohage <git@j2L4e.de>
2018-12-14 15:59:36 +0100

    add glob pattern for octave on windows, add comments

--- src/renderer/data/common-executables.js
+++ src/renderer/data/common-executables.js
@@ -1,14 +1,20 @@
 import { platform } from 'os';
 
+// You can use glob patterns here. Docs: https://github.com/isaacs/node-glob#readme
+// Be careful with too broad patterns. Scanning can take a long time.
 const commonExecutables = {
   linux: [
 
   ],
+  // darwin is macOS
   darwin: [
 
   ],
   win32: [
-
+    {
+      type: 'octave',
+      pattern: '/octave/**/octave-cli.exe',
+    },
   ],
   all: [
     {
