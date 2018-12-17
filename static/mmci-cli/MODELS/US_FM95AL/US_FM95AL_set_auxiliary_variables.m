2de71e210e4ae7102e94f8db58b03c89aaad53a6
fstrobel <31656647+fstrobel@users.noreply.github.com>
2018-12-17 17:06:47 +0100

    deletion of existing dynare files

--- static/mmci-cli/MODELS/US_FM95AL/US_FM95AL_set_auxiliary_variables.m
+++
@@ -1,7 +0,0 @@
-function y = US_FM95AL_set_auxiliary_variables(y, x, params)
-%
-% Status : Computes static model for Dynare
-%
-% Warning : this file is generated automatically by Dynare
-%           from model file (.mod)
-
