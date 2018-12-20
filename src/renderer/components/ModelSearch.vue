310c779737b9e6cea0a44c671af424ab029a0db7
Gregor Boehl <admin@gregorboehl.com>
2018-12-20 20:29:29 +0100

    label search bar

--- src/renderer/components/ModelSearch.vue
+++ src/renderer/components/ModelSearch.vue
@@ -1,7 +1,7 @@
 <template>
     <form class="ctrl-search form-inline">
         <input id="search" class="form-control form-control-sm mr-2 typeahead" autocomplete="off"
-               placeholder="Search" aria-label="Search" v-model="search" @keydown.enter.prevent>
+               placeholder="Search models..." aria-label="Search" v-model="search" @keydown.enter.prevent>
         <b-popover target="search"
                    :delay="{ show: 500, hide: 0 }"
                    placement="left"
