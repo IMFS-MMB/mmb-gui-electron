2bc096396ffd5a68cc931639263123a0896a1c40
Jan Lohage <git@j2L4e.de>
2019-02-09 19:54:57 +0100

    Show different headers in electron and web versions

--- src/renderer/components/OcpHeader.vue
+++ src/renderer/components/OcpHeader.vue
@@ -3,15 +3,18 @@
         <b-container>
             <b-row class="mt-4">
                 <b-col>
-                    <!--<h1>Online Comparison Platform</h1>-->
+                    <h1>Macroeconomic Model Database</h1>
+                    <h4 v-if="$isWeb">Online Comparison Platform</h4>
 
                     <p>
-                    Welcome to the <a href="https://www.macromodelbase.com/" target="_blank">Macroeconomic Model Database</a>
-                        <!--(MMB) Online Comparison Platform (OCP).</p> </b-col>-->
-                        (MMB) v.3.0. Contribute your model using our <a href="http://www.macromodelbase.com/form/code-submission" target="_blank">Contribute Form</a> or
-                    get in touch via our
-                    <a href="https://mmb.trydiscourse.com/" target="_blank">forum</a>.
-                        </p> </b-col>
+                        Welcome to the
+                        <a href="https://www.macromodelbase.com/" target="_blank">Macroeconomic Model Database</a>
+                        (MMB) v.3.0. Contribute your model using our
+                        <a href="http://www.macromodelbase.com/form/code-submission" target="_blank">Contribute Form</a>
+                        or get in touch via our
+                        <a href="https://mmb.trydiscourse.com/" target="_blank">forum</a>.
+                    </p>
+                </b-col>
                 <b-col>
                     <div class="mmb-branding">
                         <a href="http://www.macromodelbase.com/" target="_blank">
