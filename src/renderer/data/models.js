51514f61471bfc006c864fa8c7abec56dba86c78
Jan Lohage <git@j2L4e.de>
2019-04-24 17:22:35 +0200

    add model metadata schema, split models.json into per model .json files, load models via require.context finding all .json files matching MODELS/MODELNAME/MODELNAME.json path

---
+++ src/renderer/data/models.js
@@ -0,0 +1,5 @@
+// require all files matching the './MODELNAME/MODELNAME.json' pattern
+const ctx = require.context('@/../../static/mmci-cli/MODELS', true, /^\.\/([^/]*)\/\1\.json$/);
+const models = ctx.keys().map(key => ctx(key));
+
+export default models;
