a9c1e63b4e1292c50196962099d07af86767537f
Jan Lohage <git@j2L4e.de>
2019-04-25 16:57:43 +0200

    update tests

--- test/octave.js
+++ test/octave.js
@@ -5,21 +5,38 @@ import { assert } from 'chai';
 
 import { create } from '../src/renderer/utils/electron/interface';
 
-describe('Matlab/Octave', () => {
-  describe('Octave', () => {
-    let octave;
 
-    before(() => {
-      octave = create({
-        type: 'octave',
-        path: '/usr/bin/octave',
+describe('Matlab/Octave', () => {
+  const {
+    OCTAVE_PATH,
+    MATLAB_PATH,
+  } = process.env;
+
+  function setup(name, path) {
+    describe(name, () => {
+      let octave;
+
+      before(() => {
+        octave = create({
+          type: 'octave',
+          path,
+        });
+      });
+
+      it('can launch and detect version', async () => {
+        const pattern = /\d+\.\d+\.\d+/;
+        const version = await octave.getVersion();
+
+        assert.match(version, pattern);
       });
     });
+  }
 
-    it('can launch and detect version', async () => {
-      const pattern = /\d+\.\d+\.\d+/;
-      const version = await octave.getVersion();
-      assert.match(version, pattern);
-    });
-  });
+  if (OCTAVE_PATH) {
+    setup('Octave', OCTAVE_PATH);
+  }
+
+  if (MATLAB_PATH) {
+    setup('Matlab', MATLAB_PATH);
+  }
 });
