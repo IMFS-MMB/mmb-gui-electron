77ffb2f49bc845dadf5da64c4498f39881a8fcd5
Jan Lohage <git@j2L4e.de>
2019-04-26 09:55:48 +0200

    move stuff around

--- static/mmci-cli/MMB_OPTIONS/test.m
+++
@@ -1,5 +0,0 @@
-models = 2^34;
-rules = 2^0;
-output = 2^0;
-shocks = 2^0;
-CMD_MMB(models,rules,output,shocks,'data','[NaN,0.5,0,0;0,0.3,0.4,0;0,0,0,0;0,0,0,0;0,0,0,0;NaN,0,0,0;NaN,0,0,0;NaN,0,0,0;NaN,0,0,0]');
