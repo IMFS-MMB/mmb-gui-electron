d43411702c0c39ba4be6509aaf660e482ce1bc14
Jan Lohage <git@j2L4e.de>
2019-04-26 10:09:35 +0200

    remove MODELS folder, fix models path in config

--- config/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-dynamic-require */
-
-import path from 'path';
-
-export const mmbFolder = path.join(__dirname, '..', 'static', 'mmci-cli');
-export const modelsFolder = path.join(mmbFolder, 'MODELS');
-export const modelsSchemaPath = path.join(modelsFolder, 'model.schema.json');
