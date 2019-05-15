15401e155b03558f47baf9b3303347c56b7383cc
Jan Lohage <git@j2L4e.de>
2019-05-15 13:47:58 +0200

    extracted "vars" were "shocks" actually. move them accordingly. also extract the actual "vars". make the model schema match.

--- static/mmci-cli/models/extract.js
+++ static/mmci-cli/models/extract.js
@@ -9,12 +9,26 @@ models.forEach((model) => {
   const json = require(jsonFile);
   const mod = fs.readFileSync(modFile, { encoding: 'utf8' });
 
-  json.vars = mod
+  json.shocks = mod
     .split(/\r?\n/)
     .map(line => line.replace(/\/\/.*$/, '')) // remove // style comments
     .map(line => line.replace(/%.*$/, '')) // remove % style comments
     .join(' ')
     .match(/varexo(.*?);/)[1] // find match between 'varexo' and the first semicolon thereafter
+    .replace(/\/*.+?\*\//g, '') // remove /* */ style comments
+    .replace(/,/g, ' ')
+    .split(/\s+/) // split into non-whitespace portions
+    .filter(line => !/^\s*$/.test(line)) // remove whitespace-only lines
+    .sort((a, b) => a.localeCompare(b))
+    .reduce((acc, v) => acc.concat(v), []);
+
+  json.vars = mod
+    .split(/\r?\n/)
+    .map(line => line.replace(/\/\/.*$/, '')) // remove // style comments
+    .map(line => line.replace(/%.*$/, '')) // remove % style comments
+    .join(' ')
+    .split('varexo')[0] // everything before 'varexo'
+    .match(/var (.*?);/)[1] // find match between 'var' and the first semicolon thereafter
     .replace(/\/*.+?\*\//g, '') // remove /* */ style comments
     .replace(/,/g, ' ')
     .split(/\s+/) // split into non-whitespace portions
