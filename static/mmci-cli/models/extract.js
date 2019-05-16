3e4098319c823f5227dfdad013a38e5df78242ae
Jan Lohage <git@j2L4e.de>
2019-05-16 12:40:52 +0200

    add human_readable fields to var/shock arrays

--- static/mmci-cli/models/extract.js
+++ static/mmci-cli/models/extract.js
@@ -20,7 +20,10 @@ models.forEach((model) => {
     .split(/\s+/) // split into non-whitespace portions
     .filter(line => !/^\s*$/.test(line)) // remove whitespace-only lines
     .sort((a, b) => a.localeCompare(b))
-    .reduce((acc, v) => acc.concat(v), []);
+    .reduce((acc, shockname) => acc.concat({
+      name: shockname,
+      human_readable: shockname,
+    }), []);
 
   json.vars = mod
     .split(/\r?\n/)
@@ -34,7 +37,10 @@ models.forEach((model) => {
     .split(/\s+/) // split into non-whitespace portions
     .filter(line => !/^\s*$/.test(line)) // remove whitespace-only lines
     .sort((a, b) => a.localeCompare(b))
-    .reduce((acc, v) => acc.concat(v), []);
+    .reduce((acc, varname) => acc.concat({
+      name: varname,
+      human_readable: varname,
+    }), []);
 
   fs.writeFileSync(jsonFile, JSON.stringify(json, null, 2));
 });
