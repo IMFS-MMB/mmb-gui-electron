19d7927babcab656ee574d698b216b7ec90c6c04
Jan Lohage <git@j2L4e.de>
2019-05-28 12:14:59 +0200

    remove obsolete function

--- static/mmci-cli/lib/copy_fields.m
+++
@@ -1,9 +0,0 @@
-function result = copy_fields (source, target)
-  fields = fieldnames(source);
-
-  for i = 1:numel(fields)
-    target.(fields{i}) = source.(fields{i});
-  end
-
-  result = target;
-end
