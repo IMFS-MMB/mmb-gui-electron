674eaf6b6a8fb111d04796cb9bbf21dc8cb234e5
Jan Lohage <git@j2L4e.de>
2019-05-29 16:15:50 +0200

    fix output data format

--- static/mmci-cli/lib/save_result.m
+++ static/mmci-cli/lib/save_result.m
@@ -2,8 +2,8 @@ function save_result(modelbase, path, model_name, rule_name)
   output.model = model_name;
   output.rule = rule_name;
 
-  output.AC = struct_from_names_and_values(modelbase.AUTendo_names, @(i) modelbase.AUTR(i,:));
-  output.VAR = struct_from_names_and_values(modelbase.VARendo_names, @(i) modelbase.VAR(i,i));
+  data.AC = struct_from_names_and_values(modelbase.AUTendo_names, @(i) modelbase.AUTR(i,:));
+  data.VAR = struct_from_names_and_values(modelbase.VARendo_names, @(i) modelbase.VAR(i,i));
 
   for p = 1:size(modelbase.innos, 1)
     % 0 if shock unavailable
@@ -12,8 +12,10 @@ function save_result(modelbase, path, model_name, rule_name)
     end
 
     inno = deblank(modelbase.innos(p, :));
-    output.IRF.(inno) = struct_from_names_and_values(modelbase.IRFendo_names, @(i) modelbase.IRF(i,:,p));
+    data.IRF.(inno) = struct_from_names_and_values(modelbase.IRFendo_names, @(i) modelbase.IRF(i,:,p));
   end
+
+  output.data = data;
 
   [folder] = fileparts(path);
   mkdir(folder);
