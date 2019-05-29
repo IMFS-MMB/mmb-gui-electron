d9f61798462fc0c0b29d3cc1cd63eea5fce86e7e
Jan Lohage <git@j2L4e.de>
2019-05-29 16:14:32 +0200

    move to new data format

--- src/renderer/store/modules/comparison.js
+++ src/renderer/store/modules/comparison.js
@@ -1,5 +1,5 @@
-import groupBy from 'lodash.groupby';
-import map from 'lodash.map';
+import get from 'lodash.get';
+import pick from 'lodash.pick';
 
 import compare from './comparison.compare.PLATFORM';
 
@@ -14,44 +14,36 @@ const state = {
   error: false,
 };
 
-function toSeries(data) {
+function getSeriesId(d) {
+  return `${d.model}, ${d.rule}`;
+}
+
+function getAllSeries(data, nameSelector, dataSelector) {
   return data
     .map(d => ({
-      name: `${d.model}, ${d.rule}`,
-      data: d.values,
+      id: nameSelector(d),
+      name: nameSelector(d),
+      data: dataSelector(d.data),
     }))
-    .filter(d => !!d.data);
+    .filter(series => series.data && series.data.length);
 }
 
-function toChartData(raw, title, filter) {
-  const data1 = raw
-    .filter(filter);
-
-  if (!data1.length) {
-    return null;
-  }
-
-  const grouped = groupBy(data1, d => d.outputvar);
-
-  return [
-    {
-      title: `${title} - Inflation`,
-      series: toSeries(grouped.inflation),
-    },
-    {
-      title: `${title} - Interest Rate`,
-      series: toSeries(grouped.interest),
-    },
-    {
-      title: `${title} - Output`,
-      series: toSeries(grouped.output),
-    },
-    {
-      title: `${title} - Output Gap`,
-      series: toSeries(grouped.outputgap),
-    },
-  ];
+function getChart(data, title, dataSelector) {
+  return {
+    title,
+    series: getAllSeries(data, d => getSeriesId(d), d => dataSelector(d)),
+  };
 }
+
+function getAllCharts(allData, varNames, titleFactory, dataSelector) {
+  return varNames.map(varName => getChart(
+    allData,
+    titleFactory(varName),
+    d => dataSelector(d, varName),
+  ));
+}
+
+const DEFAULT_VARS = ['inflation', 'interest', 'output', 'outputgap'];
 
 const getters = {
   data(state) {
@@ -67,28 +59,23 @@ const getters = {
     return state.inProgress;
   },
   acCharts(state) {
-    return toChartData(state.data, 'Autocorrelation Fct.', d => d.func === 'AC');
+    return getAllCharts(state.data, DEFAULT_VARS, varName => `AC - ${varName}`, (data, varName) => get(data, ['AC', varName]));
   },
   monCharts(state) {
-    return toChartData(state.data, 'Monetary Shock', d => d.shock === 'Mon');
+    return getAllCharts(state.data, DEFAULT_VARS, varName => `Monetary Shock - ${varName}`, (data, varName) => get(data, ['IRF', 'interest_', varName]));
   },
   fisCharts(state) {
-    return toChartData(state.data, 'Fiscal Shock', d => d.shock === 'Fis');
+    return getAllCharts(state.data, DEFAULT_VARS, varName => `Fiscal Shock - ${varName}`, (data, varName) => get(data, ['IRF', 'fiscal_', varName]));
   },
   varTable(state) {
-    const varResults = state.data.filter(data => data.func === 'VAR');
+    state.data.map((d) => {
+      const vars = pick(d.data.VAR, DEFAULT_VARS);
 
-    const groupedVariances = groupBy(varResults, data => `${data.model}, ${data.rule}`);
-
-    return map(groupedVariances, (data, index) => ({
-      title: index,
-      data: {
-        inflation: (data.find(d => d.outputvar === 'inflation') || {}).values,
-        interest: (data.find(d => d.outputvar === 'interest') || {}).values,
-        output: (data.find(d => d.outputvar === 'output') || {}).values,
-        outputgap: (data.find(d => d.outputvar === 'outputgap') || {}).values,
-      },
-    }));
+      return {
+        title: getSeriesId(d),
+        data: vars,
+      };
+    });
   },
 };
 
