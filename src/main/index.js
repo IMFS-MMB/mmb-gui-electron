9856fc8fbfdf4ea405bbd1d278faf3c31d092d57
Jan Lohage <git@j2L4e.de>
2019-06-06 17:52:58 +0200

    don't focus main window when devTools were opened (flashes during development)

--- src/main/index.js
+++ src/main/index.js
@@ -54,9 +54,6 @@ function createWindow() {
   // Open dev tools initially when in development mode
   if (process.env.NODE_ENV === 'development') {
     mainWindow.webContents.on('did-frame-finish-load', () => {
-      mainWindow.webContents.once('devtools-opened', () => {
-        mainWindow.focus();
-      });
       mainWindow.webContents.openDevTools();
     });
   }
