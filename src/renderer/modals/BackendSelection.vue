1b9eefe4ecc2a687491f09b82b6637ae557148d7
Jan Lohage <git@j2L4e.de>
2019-06-10 15:45:26 +0200

    add path examples for "Find manually" buttons

--- src/renderer/modals/BackendSelection.vue
+++ src/renderer/modals/BackendSelection.vue
@@ -13,12 +13,28 @@
 
         <b-btn variant="primary" block @click="scan" :disabled="scanning">{{ scanning ? 'Scanning...' : 'Scan'}}
         </b-btn>
-        <b-btn variant="primary" block @click="find">Find manually</b-btn>
-        <b-btn variant="warning" block @click="removeSelected" :disabled="selectedIndex === null">Remove selected</b-btn>
+        <b-btn id="select-backend-manually" variant="primary" block @click="find">Find manually</b-btn>
+        <b-btn variant="warning" block @click="removeSelected" :disabled="selectedIndex === null">Remove selected
+        </b-btn>
+
+        <b-popover target="select-backend-manually"
+                   placement="top"
+                   triggers="hover"
+                   boundary="viewport">
+            <p>
+                Select the MATLAB or Octave executable.
+            </p>
+            <p>
+                Examples<br>
+                MATLAB: {{ exampleMatlab }}<br>
+                Octave: {{ exampleOctave }}
+            </p>
+        </b-popover>
     </b-card>
 </template>
 <script>
   import { mapActions, mapGetters, mapMutations } from 'vuex';
+  import { platform } from 'os';
 
   export default {
     name: 'BackendSelection',
@@ -32,6 +48,32 @@
         set(index) {
           this.select(index);
         },
+      },
+      exampleMatlab() {
+        switch (platform()) {
+          case 'darwin':
+            return '/Applications/MATLAB_R2019a.app/bin/matlab';
+          case 'linux':
+            return '/usr/local/bin/matlab';
+          case 'win32':
+            return 'C:\\Program Files\\MATLAB\\R2019a\\bin\\matlab.exe';
+
+          default:
+            throw new Error(`(unknown platform: ${platform()})`);
+        }
+      },
+      exampleOctave() {
+        switch (platform()) {
+          case 'darwin':
+            return '/Applications/Octave 4.4.1.app/bin/octave';
+          case 'linux':
+            return '/usr/bin/octave';
+          case 'win32':
+            return 'C:\\Octave\\4.5.6';
+
+          default:
+            throw new Error(`(unknown platform: ${platform()})`);
+        }
       },
     },
     methods: {
--- src/renderer/modals/DynareSelection.vue
+++ src/renderer/modals/DynareSelection.vue
@@ -3,16 +3,26 @@
         <b-card-title>Dynare</b-card-title>
 
         <b-form-select v-model="selectedOption" class="mb-3">
-            <option :value="index" v-for="(dynare, index) in dynares">{{dynare.path}} ({{dynare.version || 'unknown version'}})</option>
+            <option :value="index" v-for="(dynare, index) in dynares">{{dynare.path}} ({{dynare.version || 'unknown version'}})
+            </option>
         </b-form-select>
 
         <b-btn variant="primary" block @click="scan" :disabled="scanning">{{ scanning ? 'Scanning...' : 'Scan'}}</b-btn>
-        <b-btn variant="primary" block @click="find">Find manually</b-btn>
-        <b-btn variant="warning" block @click="removeSelected" :disabled="selectedIndex === null">Remove selected</b-btn>
+        <b-btn id="select-dynare-manually" variant="primary" block @click="find">Find manually</b-btn>
+        <b-btn variant="warning" block @click="removeSelected" :disabled="selectedIndex === null">Remove selected
+        </b-btn>
+
+        <b-popover target="select-dynare-manually"
+                   placement="top"
+                   triggers="hover"
+                   boundary="viewport">
+            Select the root folder of your dynare installation, e.g. '{{examplePath}}'
+        </b-popover>
     </b-card>
 </template>
 <script>
   import { mapActions, mapGetters, mapMutations } from 'vuex';
+  import { platform } from 'os';
 
   export default {
     name: 'DynareSelection',
@@ -26,6 +36,15 @@
         set(index) {
           this.select(index);
         },
+      },
+      examplePath() {
+        switch (platform()) {
+          case 'darwin': return '/usr/lib/dynare';
+          case 'linux': return '/usr/lib/dynare';
+          case 'win32': return 'C:\\dynare\\4.5.6';
+
+          default: throw new Error(`(unknown platform: ${platform()})`);
+        }
       },
     },
     methods: {
