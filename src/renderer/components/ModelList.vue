6c7997ca3a141c8777c459ab4dd3f84a4f30fde7
Jan Lohage <git@j2L4e.de>
2019-08-13 14:55:50 +0200

    use model.name instead of model.id for HTML id generation

--- src/renderer/components/ModelList.vue
+++ src/renderer/components/ModelList.vue
@@ -6,7 +6,7 @@
 
         <b-form-checkbox-group class="ctrl-set-listing" stacked v-model="selection">
             <template v-for="model in models">
-                <div :id="'cb-wrapper-' + model.id">
+                <div :id="'cb-wrapper-' + model.name">
                     <b-form-checkbox :value="model" :disabled="isModelDisabled(model)">{{model.name}}</b-form-checkbox>
                 </div>
 
