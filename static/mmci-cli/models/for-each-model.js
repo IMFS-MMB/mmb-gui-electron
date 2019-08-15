0e46cc082281296bf61b13766e3f38646ca71636
Jan Lohage <git@j2L4e.de>
2019-08-15 12:11:26 +0200

    refactor extraction of var-names

--- static/mmci-cli/models/for-each-model.js
+++ static/mmci-cli/models/for-each-model.js
@@ -1,19 +1,18 @@
 const fs = require('fs');
 const models = require('./models');
 
-function forEachModel(fn) {
+module.exports = function forEachModel(fn) {
   return models.forEach((model) => {
     const jsonFile = `./${model}/${model}.json`;
+    const modFile = `./${model}/${model}.mod`;
 
     // eslint-disable-next-line import/no-dynamic-require
     const json = require(jsonFile);
+    const mod = fs.readFileSync(modFile, { encoding: 'utf8' });
 
-    fn(json);
+    fn(json, mod);
 
     fs.writeFileSync(jsonFile, JSON.stringify(json, null, 2));
   });
-}
+};
 
-forEachModel((json) => {
-  console.log(`${json.name}: ${json.shocks.length}`);
-});
