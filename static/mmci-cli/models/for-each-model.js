ee1af044321a63762fd52c3c07fbfeb38bc39f83
Jan Lohage <git@j2L4e.de>
2019-08-30 10:18:59 +0200

    helper: extract vars/shocks from specific model. usage:
    cd static/mmci-cli/models
    node extract-var-names.js MY_MODEL00

--- static/mmci-cli/models/for-each-model.js
+++ static/mmci-cli/models/for-each-model.js
@@ -1,18 +1,7 @@
-const fs = require('fs');
 const models = require('./models');
+const alterModel = require('./alter-model');
 
 module.exports = function forEachModel(fn) {
-  return models.forEach((model) => {
-    const jsonFile = `./${model}/${model}.json`;
-    const modFile = `./${model}/${model}.mod`;
-
-    // eslint-disable-next-line import/no-dynamic-require
-    const json = require(jsonFile);
-    const mod = fs.readFileSync(modFile, { encoding: 'utf8' });
-
-    fn(json, mod);
-
-    fs.writeFileSync(jsonFile, JSON.stringify(json, null, 2));
-  });
+  return models.forEach(model => alterModel(model, fn));
 };
 
