f92560d90d6912f475f6dcd6e077527d95b35e82
Jan Lohage <git@j2L4e.de>
2019-09-24 16:19:13 +0200

    remove obsolete files

--- static/mmci-cli/models/for-each-model.js
+++
@@ -1,7 +0,0 @@
-const models = require('./models');
-const alterModel = require('./alter-model');
-
-module.exports = function forEachModel(fn) {
-  return models.forEach(model => alterModel(model, fn));
-};
-
