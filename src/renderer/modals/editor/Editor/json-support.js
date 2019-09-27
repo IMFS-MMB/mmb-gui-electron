937fcc815bfded8cbc0f4071b56f55a6eb620311
Jan Lohage <git@j2L4e.de>
2019-09-27 14:45:45 +0200

    delete obsolete file

--- src/renderer/modals/editor/Editor/json-support.js
+++
@@ -1,9 +0,0 @@
-import common from '../../../../schemas/common.schema.json';
-import config from '../../../../schemas/config.schema.json';
-import model from '../../../../schemas/model.schema.json';
-import rule from '../../../../schemas/rule.schema.json';
-
-
-export default function setupJsonSupport(monaco) {
-
-}
