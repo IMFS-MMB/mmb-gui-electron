0056bccb8441ee725172911c60399ed3f8ee2e92
Jan Lohage <git@j2L4e.de>
2019-12-07 13:26:07 +0100

    fix USR sometimes not being applied

--- static/mmci-cli/lib/run_model_with_rules.m
+++ static/mmci-cli/lib/run_model_with_rules.m
@@ -7,7 +7,13 @@ function run_model_with_rules(model, rules, config, paths)
   copyfile(fullfile(paths.models, model.name), fullfile(paths.work, model.name));
 
   % run with user specified rule
-  if (iscell(config.usr))
+  if (iscell(config.usr) || size(config.usr, 2) > 1)
+    if (iscell(config.usr))
+      usr = config.usr;
+    else
+      usr = num2cell(config.usr);
+    end
+
     rule = make_rule('User', config.usr);
 
     result = run_dynare_and_simulate(model, rule, config.options, paths.work);
