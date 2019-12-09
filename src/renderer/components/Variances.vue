c59f6a232fed96d3b21031a90e2a5937ee2ac1d4
Jan Lohage <git@j2L4e.de>
2019-12-09 11:20:01 +0100

    fix var table overflow by making it scrollable

--- src/renderer/components/Variances.vue
+++ src/renderer/components/Variances.vue
@@ -1,20 +1,22 @@
 <template>
     <b-row class="mt-5">
         <b-col>
-            <table class="table table-bordered table-striped">
-                <thead>
-                <tr>
-                    <th>Variances (all quarterly)</th>
-                    <th v-for="header of varTable.header">{{header}}</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="row of varTable.rows">
-                    <th>{{row.title}}</th>
-                    <td v-for="val of row.values">{{val}}</td>
-                </tr>
-                </tbody>
-            </table>
+            <div id="variances-container" class="pretty-scroll">
+                <table id="variances-table" class="table table-bordered table-striped">
+                    <thead>
+                    <tr>
+                        <th>Variances (all quarterly)</th>
+                        <th v-for="header of varTable.header">{{header}}</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="row of varTable.rows">
+                        <th>{{row.title}}</th>
+                        <td v-for="val of row.values">{{val}}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </b-col>
     </b-row>
 </template>
@@ -28,3 +30,22 @@
     },
   };
 </script>
+<style lang="scss">
+    #variances-container {
+        overflow-x: auto;
+        padding-left: 0;
+        margin-left: 20px;
+    }
+
+    #variances-table {
+        border-spacing: 0;
+        border: 0;
+        border-collapse: separate;
+
+        th:first-of-type{
+            position: sticky;
+            background: white;
+            left: 0;
+        }
+    }
+</style>
