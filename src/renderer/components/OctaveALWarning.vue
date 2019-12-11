92f834513b71c615e60dfd2a8fdac08f761f2fb7
Jan Lohage <git@j2L4e.de>
2019-12-11 09:45:47 +0100

    AL warning: show for var or ac selection only

--- src/renderer/components/OctaveALWarning.vue
+++ src/renderer/components/OctaveALWarning.vue
@@ -2,7 +2,7 @@
     <b-row>
         <b-col>
             <b-alert class="m-0" variant="warning" :show="showWarning">
-                AL model warning here.
+                <b>Warning:</b> calculating AC functions and/or variances will take about 10 minutes per model/policy rule combination
             </b-alert>
         </b-col>
     </b-row>
@@ -16,15 +16,22 @@
       ...mapGetters('backends', {
         selectedBackend: 'selected',
       }),
-      ...mapGetters('options', ['models']),
+      ...mapGetters('options', [
+        'models',
+        'plotAutocorrelation',
+        'plotVariance',
+      ]),
       isOctave() {
         return !!this.selectedBackend && this.selectedBackend.type === InterfaceType.Octave;
       },
       isALModel() {
         return this.models.some(model => !!model.al);
       },
+      isACOrVAR() {
+        return this.plotAutocorrelation || this.plotVariance;
+      },
       showWarning() {
-        return this.isOctave && this.isALModel;
+        return this.isOctave && this.isALModel && this.isACOrVAR;
       },
     },
   };
