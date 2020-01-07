30f69625d4b22c31a8e4e0eeb70eac9d6a862c81
Jan Lohage <git@j2L4e.de>
2020-01-07 12:27:10 +0100

    ocp: update crunch script

--- .scripts/make-crunch.js
+++ .scripts/make-crunch.js
@@ -10,19 +10,22 @@ const rootFolder = path.resolve(__dirname, '..');
 const modelsFolder = path.resolve(rootFolder, 'static', 'mmci-cli', 'models');
 const tmpFolder = path.resolve(rootFolder, 'tmp');
 
+const DYNARE_PATH = '/usr/lib/dynare';
+
 const configBase = {
-  'dynare': '/usr/lib/dynare',
-  'rules': null,
-  'models': null,
-  'msr': null,
-  'usr': false,
-  "shocks": [
-    "interest_",
-    "fiscal_"
-  ],
-  'options': {
-    'gain': 0.01,
-    'horizon': 20
+  dynare: DYNARE_PATH,
+  rules: null,
+  models: null,
+  msr: null,
+  usr: false,
+  options: {
+    shocks: [
+      'interest_',
+      'fiscal_'
+    ],
+    gain: 0.01,
+    horizon: 20,
+    states: {},
   }
 };
 
